7d56b55bf71a795b9912ebd5b521c602dce8b899
MrGamingBoss <sparsh.mogha@gmail.com>
2025-10-17 03:23:57 -0400

    Revert "Fixed Dark Mode in Website in the home page, board, and the resources page"
    This reverts commit 8cd7ac674ec16f87325969f7b4f3ba463bffb56c.

--- src/client/routes/index.tsx
+++ src/client/routes/index.tsx
@@ -100,7 +100,7 @@ export const Route = createFileRoute("/")({
           </div>
         </div>
 
-        <div className="flex w-full flex-col items-center bg-saseGray p-12 dark:bg-background">
+        <div className="flex w-full flex-col items-center bg-saseGray p-12 dark:bg-greenBackground">
           <h1 className="pb-12 text-center font-oswald text-6xl font-medium">Our Mission</h1>
           <div className="grid gap-12 pl-4 pr-4 sm:grid-cols-1 md:grid-cols-3">
             <MissionCard
@@ -191,10 +191,10 @@ export const Route = createFileRoute("/")({
           <Carousel prog="N/A" purpose="Values" />
         </div>
 
-        <div className="flex w-full flex-col items-center justify-center bg-saseGray p-10 dark:bg-background">
+        <div className="flex w-full flex-col items-center justify-center bg-saseGray p-10 dark:bg-greenBackground">
           <h1 className="w-full pb-12 text-center font-oswald text-6xl font-medium">Sponsors</h1>
           <div className="mb-20 flex w-10/12 flex-col items-start gap-14 lg:flex-row xl:gap-24">
-            <div className="flex w-full flex-col items-center rounded-2xl border-4 border-foreground bg-muted p-10 shadow-[12px_12px_0px_#7DC242]">
+            <div className="flex w-full flex-col items-center rounded-2xl border-4 border-border bg-muted p-10 shadow-[12px_12px_0px_#7DC242]">
               <p className="p-4 text-left font-redhat text-xl sm:text-2xl">
                 Are you interested in becoming a partner with the UF Society of Asian Scientists and Engineers (SASE) Chapter?
                 <br />
